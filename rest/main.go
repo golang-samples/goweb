643d3ede1a2101a3d1e7b38e49c6bcb4c4ab64b6
Takuya Ueda <uedatakuya@gmail.com>
2013-04-13 07:16:32 +0900

    add rest controller sample

---
+++ rest/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchrcom/goweb/goweb"
+)
+
+type MyController struct{}
+
+func (cr *MyController) Read(id string, cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Read resource %s", id)
+}
+
+func (cr *MyController) ReadMany(cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Read many resources")
+}
+
+func (cr *MyController) Update(id string, cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Update resource %s", id)
+}
+
+func (cr *MyController) UpdateMany(cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Update many resources")
+}
+
+func (cr *MyController) Delete(id string, cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Delete resource %s", id)
+}
+
+func (cr *MyController) DeleteMany(cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Delete many resources")
+}
+
+func main() {
+	controller := new(MyController)
+	goweb.MapRest("/api", controller)
+	goweb.ListenAndServe(":8080")
+}
